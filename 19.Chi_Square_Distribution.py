099a0bbe88dd8d2350f0ec43dacaf37c76b47ead
heysushil <sushil.chaudhary111@gmail.com>
2020-08-18 18:57:24 +0530

    Chi Square Distribution

--- 19.Chi_Square_Distribution.py
+++ 19.Chi_Square_Distribution.py
@@ -14,3 +14,47 @@ Chi Square Distribution method ke arguments:
 df: number of degree of freedom and must be >0
 size: scalar array ka shape
 '''
+
+import numpy.random as r
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# chiVaraible = r.chisquare(df=2, size=(10,10))
+# print('\nchiVaraible: \n',chiVaraible)
+
+# sns.distplot(chiVaraible, hist=False)
+# plt.show()
+
+
+# show advance example
+# pip install scipy
+# pip list
+from scipy import stats
+import numpy as np
+
+# np.linspace(star, end, randomNumber)
+numpyMatrix = np.linspace(0, 10, 100)
+# print('\nnumpyMatrix: ',numpyMatrix)
+fig, ax = plt.subplots(1, 1)
+print('\nfig: ',fig, ' \nax: ',ax)
+
+# 4 alag line ke liye dots
+linestypes = [':','--','-.','-']
+deg_of_freedom = [1,4,7,6]
+
+# for loop ka use kare line styel aur deg of freemo ko marg karna hai aur ek final matrix bana hai.
+for df, ls in zip(deg_of_freedom, linestypes):
+    ax.plot(numpyMatrix, stats.chi2.pdf(numpyMatrix, df), linestyle=ls)
+
+# x aur y axis ka range set kar rahe hain
+plt.xlim = (0, 10)
+plt.ylim(0, 0.4)
+
+# x aur y axis ka name aur output table ka name
+plt.xlabel('Values')
+plt.ylabel('Frequecy')
+plt.title('Chi_square Distribution')
+
+# show the graph
+plt.legend()
+plt.show()
